81674ddfa0c40fbcd193397bc85ba4013902ce00
imran <31067252+ProgPassion@users.noreply.github.com>
2022-10-28 12:02:58 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,17 +1,20 @@
 public class App {
     public static void main(String[] args) {
         Game game = new Game();
+        char winner;
         while(!game.over) {
             game.printMap();
             game.getPlayerInput();
             game.checkGameStatus();
         }
         game.printMap();
-        System.out.println();
-        System.out.println("Winner is: " + game.getWinner());
-        
+        winner = game.getWinner();
+
+        if(winner == 'D') 
+            System.out.println("No winner, it's a draw!");
+        else 
+            System.out.println("Winner is: " + winner); 
     }
-    
 }
     
 
